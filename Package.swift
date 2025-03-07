f4b0e4f0c90cb6eeaf2fe3fbe32ea40c808bbcde
Tomasz Rahn <trahn@simplito.com>
2025-03-07 12:43:49 +0100

    Start depending on dev branch for the dev branch

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
 	dependencies:[
 		.package(
 			url:"https://github.com/simplito/privmx-endpoint-swift",
-			.upToNextMinor(from: .init(2, 2, 0,prereleaseIdentifiers: ["rc3"]))
+			//.upToNextMinor(from: .init(2, 3, 0,prereleaseIdentifiers: ["rc1"]))
+			branch:"dev"
 		),
 	],
     targets: [
