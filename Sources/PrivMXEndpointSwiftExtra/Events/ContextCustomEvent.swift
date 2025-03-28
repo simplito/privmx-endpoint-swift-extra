ecded32bf7bf64a85e006e4a1975d14d9a667069
Tomasz Rahn <trahn@simplito.com>
2025-03-28 11:49:47 +0100

    Added Extension fot ContextCustomEvent

---
+++ Sources/PrivMXEndpointSwiftExtra/Events/ContextCustomEvent.swift
@@ -0,0 +1,63 @@
+//
+// PrivMX Endpoint Swift Extra
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import PrivMXEndpointSwiftNative
+
+/// A helper extension for `InboxCreatedEvent` to conform to the `PMXEvent` protocol.
+/// This extension is designed to assist with event channels type conversions,
+/// as channels are identified by strings in the Low-Level Endpoint.
+extension privmx.endpoint.event.ContextCustomEvent: PMXEvent {
+
+	/// Handles the event by calling the provided callback with an optional argument.
+	///
+	/// This implementation passes the `data` property to the callback.
+	/// - Parameter cb: A closure that accepts an optional `Any?` argument,
+	///   representing the data to be passed when the event is handled.
+	public func handleWith(
+		cb: (@escaping @Sendable @MainActor (Any?) async -> Void)
+	) {
+		Task{
+			await cb(data)
+		}
+	}
+
+	/// Returns the event type as a string.
+	///
+	/// This method returns the constant string `"inboxCreated"`, identifying the type
+	/// of this event as `inboxCreated`.
+	/// - Returns: A `String` representing the event type, in this case, `"inboxCreated"`.
+	public static func typeStr() -> String {
+		"contextCustom"
+	}
+
+	/// Returns the event channel as a string.
+	///
+	/// This implementation returns the constant string `"inbox"`,
+	/// identifying the channel associated with `InboxCreatedEvent`.
+	/// - Returns: A `String` representing the event channel, in this case, `"inbox"`.
+	public func getChannel() -> String {
+		String(privmx.getChannelFrom(self))
+	}
+}
+
+
+
+extension privmx.endpoint.event.ContextCustomEvent: @unchecked  Sendable {
+}
+
+
+
+
+
+
+
+
