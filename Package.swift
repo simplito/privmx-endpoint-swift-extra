3dfaa57f0b5062c42ac847fa592b84d89a4dd7d0
Tomasz Rahn <trahn@simplito.com>
2025-06-25 15:01:45 +0200

    Using dev branch of PES

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	],
 	dependencies:[
 		.package(url:"https://github.com/simplito/privmx-endpoint-swift",
-				 .upToNextMinor(from: .init(2, 3, 0,prereleaseIdentifiers: ["rc1"]))
+				 branch: "dev"
 				 ),
 	],
     targets: [
