9519a5015fcbc4b95f89f5492d1c6e4a5eaa6b97
Tomasz Rahn <trahn@simplito.com>
2025-07-03 14:18:47 +0200

    Depend on 2.5.0-rc1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	],
 	dependencies:[
 		.package(url:"https://github.com/simplito/privmx-endpoint-swift",
-				 branch: "dev"
+				 .upToNextMinor(from: .init(2, 5, 0, prereleaseIdentifiers: ["rc1"]))
 				 ),
 	],
     targets: [
