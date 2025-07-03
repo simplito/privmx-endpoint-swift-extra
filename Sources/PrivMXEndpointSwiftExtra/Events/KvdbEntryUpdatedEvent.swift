fc072c4f38f0c667850670ed7e3f3e90c15a0e80
Tomasz Rahn <trahn@simplito.com>
2025-07-03 12:38:36 +0200

    Feat: Key-Value Database (#66)
    * KvdbApi extension + update of PagingQuery convenience init
    
    * Events and PrivMXEndpoint update
    
    * Fix: typeStr implementation in ThreadMessageUpdated now correctly returns 'threadMessageUpdated'
    
    * Update Package.swift
    
    * Fixed typos
    
    * Event handling, PrivMXEndpoint inits for KVDB
    
    * Apply suggestions from code review
    
    Co-authored-by: djenczewski <149203207+djenczewski@users.noreply.github.com>

---
+++ Sources/PrivMXEndpointSwiftExtra/Events/KvdbEntryUpdatedEvent.swift
@@ -0,0 +1,51 @@
+//
+// PrivMX Endpoint Swift Extra
+// Copyright © 2025 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import PrivMXEndpointSwiftNative
+
+/// A helper extension for `KvdbEntryUpdatedEvent` to conform to the `PMXEvent` protocol.
+/// This extension is designed to assist with event channels type conversions,
+/// as channels are identified by strings in the Low-Level Endpoint.
+extension privmx.endpoint.kvdb.KvdbEntryUpdatedEvent: PMXEvent, @unchecked Sendable {
+
+	/// Returns the event channel as a string.
+	///
+	/// This implementation returns the string in the format `"kvdb/{kvdbId}/entries"`,
+	/// where `kvdbId` is obtained from the `data.kvdbId` property.
+	/// - Returns: A `String` representing the event channel, in this case, `"kvdb/{kvdbId}/entries"`.
+	public func getChannel() -> String {
+		"kvdb/\(self.data.info.kvdbId)/entries"
+	}
+
+	/// Handles the event by calling the provided callback with an optional argument.
+	///
+	/// This implementation passes the `data` property to the callback.
+	/// - Parameter cb: A closure that accepts an optional `Any?` argument,
+	///   representing the data to be passed when the event is handled.
+	public func handleWith(
+		cb: @escaping (@MainActor @Sendable (_ data: Any?)  async -> Void)
+	) -> Void {
+		Task{
+			await cb(data)
+		}
+	}
+
+	/// Returns the event type as a string.
+	///
+	/// This method returns the constant string `"kvdbEntryUpdated"`, identifying the type
+	/// of this event as `kvdbEntryUpdated`.
+	/// - Returns: A `String` representing the event type, in this case, `"kvdbEntryUpdated"`.
+	public static func typeStr() -> String {
+		"kvdbEntryUpdated"
+	}
+}
+
---
+++ Sources/PrivMXEndpointSwiftExtra/Events/KvdbNewEntryEvent.swift
@@ -0,0 +1,50 @@
+//
+// PrivMX Endpoint Swift Extra
+// Copyright © 2024 Simplito sp. z o.o.
+//
+// This file is part of the PrivMX Platform (https://privmx.dev).
+// This software is Licensed under the MIT License.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import PrivMXEndpointSwiftNative
+
+/// A helper extension for `KvdbNewEntryEvent` to conform to the `PMXEvent` protocol.
+/// This extension is designed to assist with event channels type conversions,
+/// as channels are identified by strings in the Low-Level Endpoint.
+extension privmx.endpoint.kvdb.KvdbNewEntryEvent: PMXEvent, @unchecked Sendable {
+
+	/// Returns the event channel as a string.
+	///
+	/// This implementation returns the string in the format `"kvdb/{kvdbId}/entries"`,
+	/// where `kvdbId` is obtained from the `data.info.kvdbId` property.
+	/// - Returns: A `String` representing the event channel, in this case, `"kvdb/{kvdbId}/entries"`.
+	public func getChannel() -> String {
+		return "kvdb/\(self.data.info.kvdbId)/entries"
+	}
+
+	/// Returns the event type as a string.
+	///
+	/// This method returns the constant string `"kvdbNewEntry"`, identifying the type
+	/// of this event as `kvdbNewEntry`.
+	/// - Returns: A `String` representing the event type, in this case, `"kvdbNewEntry"`.
+	public static func typeStr() -> String {
+		return "kvdbNewEntry"
+	}
+
+	/// Handles the event by calling the provided callback with an optional argument.
+	///
+	/// This implementation passes the `data` property to the callback.
+	/// - Parameter cb: A closure that accepts an optional `Any?` argument,
+	///   representing the data to be passed when the event is handled.
+	public func handleWith(
+		cb: @escaping (@MainActor @Sendable (_ data: Any?) async -> Void)
+	) -> Void {
+		Task{
+			await cb(data)
+		}
+	}
+}
