83ee5870effc543ff23deb5cd834fcaf7537f626
Tomasz Rahn <trahn@simplito.com>
2025-08-21 15:27:32 +0200

    Fix!:proper conformance to Hashable in extensions

--- Sources/PrivMXEndpointSwiftExtra/Extensions/FileVector.swift
+++ Sources/PrivMXEndpointSwiftExtra/Extensions/FileVector.swift
@@ -15,9 +15,9 @@ import PrivMXEndpointSwiftNative
 /// An extension for `StringVector` to conform to the `Hashable` protocol.
 /// This extension allows comparing two `StringVector` instances and generating
 /// a hash value for usage in hash-based collections, such as dictionaries or sets.
-extension privmx.StringVector: Hashable {
+extension privmx.FileVector: Hashable {
 
-	/// Compares two `StringVector` instances for equality.
+	/// Compares two `FileVector` instances for equality.
 	///
 	/// This function compares the contents of two `StringVector` instances by calling the
 	/// `privmx.compareVectors` function.
@@ -25,7 +25,7 @@ extension privmx.StringVector: Hashable {
 	///   - lhs: The left-hand side `StringVector` instance.
 	///   - rhs: The right-hand side `StringVector` instance.
 	/// - Returns: `true` if both vectors are equal, otherwise `false`.
-	public static func == (lhs: privmx.StringVector, rhs: privmx.StringVector) -> Bool {
+	public static func == (lhs: privmx.FileVector, rhs: privmx.FileVector) -> Bool {
 		privmx.compareVectors(lhs, rhs)
 	}
 
